f4c0ae0e3855f2d234027a2880fd3445e7746e11
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2024-01-24 22:37:45 +0100

    Add texts and logos

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ import 'reveal.js/dist/reveal.css';
 import './edhouse.scss';
 
 import Reveal from 'reveal.js';
+import RevealNotes from 'reveal.js/plugin/notes/notes'
 
 const deck = new Reveal();
 
-deck.initialize();
+deck.initialize({
+  plugins: [RevealNotes]
+});
