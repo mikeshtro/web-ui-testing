a177300e7de094ffebd8894e1acaaf114d789676
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2024-01-25 09:44:14 +0100

    Final presentation

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import 'reveal.js/dist/reveal.css';
+import 'reveal.js/plugin/highlight/monokai.css';
 import './edhouse.scss';
 
 import Reveal from 'reveal.js';
-import RevealNotes from 'reveal.js/plugin/notes/notes'
+import RevealNotes from 'reveal.js/plugin/notes/notes';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
 
 const deck = new Reveal();
 
 deck.initialize({
-  plugins: [RevealNotes]
+  plugins: [RevealNotes, RevealHighlight]
 });
